726093dd4f5f10b95468fd273a2df58441dcf50a
Sofia Azzahra <sofiaazzahra0001@gmail.com>
2024-11-12 14:37:07 +0700

    pertemuan 6

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+// import 'package:aslab_travel/latihanmodul/pertemuan5.dart';
+import 'package:aslab_travel/latihanmodul/pertemuan6.dart';
+import 'package:aslab_travel/screens/SplashScreen.dart';
+import 'package:aslab_travel/screens/landing_page.dart';
+import 'package:flutter/material.dart';
+void main(){
+  runApp(TravelApp());
+}
+
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner : false,
+      home: LandingPageOne(),
+    );
+  }
+}
