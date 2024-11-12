107240a15805074f899091630c55e4e31fc10a53
Sofia Azzahra <sofiaazzahra0001@gmail.com>
2024-11-12 14:42:42 +0700

    pertemuan 6

--- aslab_travel/lib/main.dart
+++ aslab_travel/lib/main.dart
@@ -1,4 +1,7 @@
+// import 'package:aslab_travel/latihanmodul/pertemuan5.dart';
+import 'package:aslab_travel/latihanmodul/pertemuan6.dart';
 import 'package:aslab_travel/screens/SplashScreen.dart';
+import 'package:aslab_travel/screens/landing_page.dart';
 import 'package:flutter/material.dart';
 void main(){
   runApp(TravelApp());
@@ -11,7 +14,7 @@ class TravelApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner : false,
-      home: SplashScreen(),
+      home: LandingPageOne(),
     );
   }
 }
